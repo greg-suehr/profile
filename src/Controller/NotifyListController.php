2de20a42a017f219b072b18b15ed7218917aeeac
gregsuehr <gregory.suehr@gmail.com>
2025-02-20 11:53:39 -0500

    Added landing page easter egg via  parameter

--- src/Controller/NotifyListController.php
+++ src/Controller/NotifyListController.php
@@ -3,21 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class NotifyListController extends AbstractController
 {
     #[Route('/', name: 'app_notify_list')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-#        return $this->render('notify_list/index.html.twig', [
-#            'controller_name' => 'NotifyListController',
-#        ]);
+      $greet = '<h1>Welcome!</h1>';
+      if ($name = $request->query->get('hello')) {
+        $greet = sprintf('<h1>Hey %s!</h1>', htmlspecialchars($name));
+      }
       return new Response(<<<EOF
                           <html>
                             <body>
-                              <h1>Welcome!</h1>
+                              $greet
                               <p>This page is still under construction.</p>
                               <div><img src="/images/construction.webp"/>
                               <p>We appreciate your patience!</p>
