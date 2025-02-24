af66c622c50a14384aec4f7d943782ac28c1ef73
gregsuehr <dev@null.com>
2025-02-24 12:47:33 -0500

    Basic Twig template for viewing the current NotifyList

--- src/Controller/NotifyListController.php
+++ src/Controller/NotifyListController.php
@@ -2,30 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Twig\Environment;
 
 final class NotifyListController extends AbstractController
 {
     #[Route('/', name: 'app_notify_list')]
-    public function index(Request $request): Response
-    {
-      $greet = '<h1>Welcome!</h1>';
-      if ($name = $request->query->get('hello')) {
-        $greet = sprintf('<h1>Hey %s!</h1>', htmlspecialchars($name));
-      }
-      return new Response(<<<EOF
-                          <html>
-                            <body>
-                              $greet
-                              <p>This page is still under construction.</p>
-                              <div><img src="/images/construction.webp"/>
-                              <p>We appreciate your patience!</p>
-                            </body>
-                          </html>
-                          EOF
-      );
-    }
+  public function index(Environment $twig, RecipientRepository $recipientRepository): Response
+  {
+    return new Response($twig->render('notify_list/index.html.twig', [
+      'recipients' => $recipientRepository->findAll(),
+    ]));
+  }
 }
