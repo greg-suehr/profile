6cb9e71f0a5c3c2c256b4779dc102b9a743691cf
gregsuehr <dev@null.com>
2025-03-10 13:38:38 -0400

    Fix typo in assets/bootstrap.js

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -3,4 +3,4 @@ import './controllers';
 
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
-..app.register('some_controller_name', SomeImportedController);
+// app.register('some_controller_name', SomeImportedController);
