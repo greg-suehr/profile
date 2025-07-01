ac2d6ecd476a3b45a10ae5d95d0cc04fc4240101
greg-suehr <gregory.suehr@gmail.com>
2025-07-01 15:32:50 -0400

    Hyperlink story base

---
+++ src/Controller/HyperLinkController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HyperLinkController extends AbstractController
+{
+    #[Route('/story', name: 'hyperlink_index')]
+    public function index(SessionInterface $session): Response
+    {
+        $storyNodeKey = $session->get('story.currentNode', 'prologue1');
+        
+        return $this->render('hyper_link/story.html.twig', [
+            'storyNodeKey' => $storyNodeKey
+        ]);
+    }
+
+    #[Route('/hyperlink/jump/{nextNodeKey}', name: 'hyperlink_jump')]
+    public function jump(string $nextNodeKey, SessionInterface $session): Response
+    {
+        $session->set('story.currentNode', $nextNodeKey);
+
+        return $this->redirectToRoute('hyperlink_index');
+    }
+
+    #[Route('/story/clear', name: 'hyperlink_clear')]
+    public function clear(SessionInterface $session): Response
+    {
+	$session->set('story.currentNode', 'prologue1');
+
+	return $this->redirectToRoute('hyperlink_index');
+    }
+}
