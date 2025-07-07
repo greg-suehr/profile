f656f8d4fc5a8753be3bc2093ba98b4079f19f67
greg-suehr <gregory.suehr@gmail.com>
2025-07-07 16:43:10 -0400

    fix: namespace

--- src/Podcast/PodcastController.php
+++ src/Podcast/PodcastController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Podcast\Controller;
+namespace App\Podcast;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
