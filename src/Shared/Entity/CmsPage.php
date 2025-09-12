bc6715f8da0e48a1d4d4ef391d468066f8fb0a92
greg-suehr <gregory.suehr@gmail.com>
2025-09-12 16:25:35 -0400

    fix

--- src/Shared/Entity/CmsPage.php
+++ src/Shared/Entity/CmsPage.php
@@ -2,7 +2,7 @@
 
 namespace App\Shared\Entity;
 
-use App\Repository\CmsPageRepository;
+use App\Shared\Repository\CmsPageRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
