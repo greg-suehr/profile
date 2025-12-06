0aeebd21746b80e1e797496a633a4738a4773d3b
greg-suehr <gregory.suehr@gmail.com>
2025-12-06 15:44:13 -0500

    Beal portfolio seed

---
+++ src/Shared/Controller/ClientController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Shared\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ClientController extends AbstractController
+{
+  #[Route('/beal', name: 'client_landing')]
+  public function landing(Request $request): Response
+  {
+    $host = $request->getHost();
+    # TODO: host routing
+
+    return $this->render('client/home.html.twig');
+  }
+
+  #[Route('/beal/bio', name: 'client_bio')]
+  public function bio(Request $request): Response { return $this->render('client/bio.html.twig'); }
+
+  #[Route('/beal/contact', name: 'client_contact')]
+  public function contact(Request $request): Response { return $this->render('client/contact.html.twig'); }
+
+  
+}
