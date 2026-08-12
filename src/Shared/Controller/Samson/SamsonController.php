82dcd8117c1c3da4e58757072d6f8854bc54b698
Greg Suehr <suehr@trustarts.org>
2026-08-12 14:59:30 -0400

    seertechne: fix vague routing

--- src/Shared/Controller/Samson/SamsonController.php
+++ src/Shared/Controller/Samson/SamsonController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+#[Route(condition: "request.getHost() matches '%samson_match%'")]
 class SamsonController extends AbstractController
 {  
   public function __construct(    
